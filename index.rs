18d036808c69771fd11a34a2f0c12117836024d7
Ilse Hernandez <serenilka@hotmail.com>
2019-09-02 18:53:37 -0600

    creation of first key

--- index.rs
+++ index.rs
@@ -1,8 +1,11 @@
 fn main() {
-    let p8: [u8; 8] = [6,3,7,4,8,5,10,9];
     let ten_bit: [bool; 10] = [true, false, true, false, false, false, false, false, true, false];
-    let permute_ten = permute_key(ten_bit);
-    println!("{:?}", permute_ten);
+    let mut permute_ten = permute_ten_key(ten_bit);
+    let (first_five, second_five) : (&mut[bool], &mut[bool]) = permute_ten.split_at_mut(5);
+    first_five.rotate_left(1);
+    second_five.rotate_left(1);
+    let permute_eight = permute_eight_key(permute_ten);
+    println!("{:?}",permute_eight);
     //let bit_key = key_to_string();
     // let mut h1 = String::from(&bit_key[0..5]);
     // let mut h2 = String::from(&bit_key[5..10]);
@@ -18,7 +21,7 @@ fn main() {
 
 }
 
-fn permute_key(ten_bit: [bool; 10]) -> [bool; 10]{
+fn permute_ten_key(ten_bit: [bool; 10]) -> [bool; 10]{
     let mut new_10_key: [bool; 10] = [false;10];
     let p10: [usize; 10] = [3,5,2,7,4,10,1,9,8,6];
     let mut i = 0;
@@ -29,6 +32,14 @@ fn permute_key(ten_bit: [bool; 10]) -> [bool; 10]{
     return new_10_key;
 }
 
-// fn key_to_string() -> String{
-
-// }
+fn permute_eight_key(ten_bit: [bool; 10]) -> [bool; 8]{
+    let mut new_8_key: [bool; 8] = [false;8];
+    println!("{:?}", ten_bit);
+    let p8: [usize; 8] = [6,3,7,4,8,5,10,9];
+    let mut i = 0;
+    for x in p8.iter(){
+        new_8_key[i] = ten_bit[*x-1];
+        i+=1;
+    }
+    return new_8_key;
+}
